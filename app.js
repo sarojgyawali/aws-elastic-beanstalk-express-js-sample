b82ccc386184efef50532497ca6351aae390036b
Saroj Gyawali <saroj.gyawalis4@gmail.com>
2021-04-24 02:30:47 -0400

    New updated changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing AWS CI/CD'));
+app.get('/', (req, res) => res.send('Testing AWS CI/CD - Version 2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
